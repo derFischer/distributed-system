d9440aae1d6718d7e1d067ce3f4e0e033da6e694
derFischer <dingd2015@sjtu.edu.cn>
2019-03-16 01:24:50 +0800

    Update rdt_sender.cc

--- rdt/rdt_sender.cc
+++ rdt/rdt_sender.cc
@@ -6,13 +6,12 @@
  *       situations.  In this implementation, the packet format is laid out as 
  *       the following:
  *       
- *       |<-  1 byte  ->|<-             the rest            ->|
- *       | payload size |<-             payload             ->|
+ *  |<-     1 byte      ->|<-     2 byte      ->|<-         4 byte          ->|<-  1 byte  ->|<-             the rest            ->|
+ *  |<-     first packet->|<-     checksum    ->|<-     sequence number     ->| payload size |<-             payload             ->|
  *
  *       The first byte of each packet indicates the size of the payload
  *       (excluding this single-byte header)
  */
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,6 +20,78 @@
 #include "rdt_struct.h"
 #include "rdt_sender.h"
 
+#define HEADERSIZE 8
+#define MAXPAYLOADSIZE (RDT_PKTSIZE - HEADERSIZE）
+#define WINDOWSIZE 3
+#define BUFFERSIZE 1000
+
+packet* tmp_buffer;
+packet* windows;
+int sequence_number = 0;
+int windowStart = 0;
+int bufferNum = 0;
+int windowNum = 0;
+
+//helper
+short checksum(char *message)
+{
+    return 0;
+}
+
+void addFirstPacket(char *packet, bool first, bool end)
+{
+    if (first && end)
+    {
+        packet[0] = 3;
+    }
+    else if (first)
+    {
+        packet[0] = 1;
+    }
+    else if (end)
+    {
+        packet[0] = 2;
+    }
+    else
+    {
+        packet[0] = 0;
+    } 
+}
+
+void addChecksum(char *packet, short checksum)
+{
+    packet[1] = (checksum)&0xFF;
+    packet[2] = (checksum >> 8) & 0xFF;
+}
+
+void addSequencenumber(char *packet)
+{
+    ((int *)(packet + 3))[0] = sequence_number++;
+}
+
+void addPayloadsize(char *packet, int payload)
+{
+    packet[7] = payload & 0xFF;
+}
+
+void package(char *packet, int payload, bool first, bool end)
+{
+    addSequencenumber(packet);
+    addPayloadsize(packet, payload);
+    addFirstPacket(packet, first, end);
+    addChecksum(packet, checksum(packet));
+}
+
+bool canSend(packet pkt)
+{
+    int seq = ((int *)(pkt.data + 3))[0];
+    return (seq < windowStart + WINDOWSIZE);
+}
+
+void addToBuffer(packet pkt)
+{
+    tmp_buffer[bufferNum++] = pkt;
+}
 
 /* sender initialization, called once at the very beginning */
 void Sender_Init()
@@ -41,11 +112,6 @@ void Sender_Final()
    sender */
 void Sender_FromUpperLayer(struct message *msg)
 {
-    /* 1-byte header indicating the size of the payload */
-    int header_size = 1;
-
-    /* maximum payload size */
-    int maxpayload_size = RDT_PKTSIZE - header_size;
 
     /* split the message if it is too big */
 
@@ -54,27 +120,45 @@ void Sender_FromUpperLayer(struct message *msg)
 
     /* the cursor always points to the first unsent byte in the message */
     int cursor = 0;
+    bool first = true;
+    bool end = false;
 
-    while (msg->size-cursor > maxpayload_size) {
-	/* fill in the packet */
-	pkt.data[0] = maxpayload_size;
-	memcpy(pkt.data+header_size, msg->data+cursor, maxpayload_size);
-
-	/* send it out through the lower layer */
-	Sender_ToLowerLayer(&pkt);
-
-	/* move the cursor */
-	cursor += maxpayload_size;
+    while (msg->size - cursor > MAXPAYLOADSIZE)
+    {
+        /* fill in the packet */
+        memcpy(pkt.data + HEADERSIZE, msg->data + cursor, MAXPAYLOADSIZE);
+        package(pkt.data, MAXPAYLOADSIZE, first, end);
+        /* send it out through the lower layer */
+        if (canSend(pkt))
+        {
+            Sender_ToLowerLayer(&pkt);
+        }
+        else
+        {
+            addToBuffer(pkt);
+        }
+        /* move the cursor */
+        cursor += maxpayload_size;
+        first = false;
     }
 
     /* send out the last packet */
-    if (msg->size > cursor) {
-	/* fill in the packet */
-	pkt.data[0] = msg->size-cursor;
-	memcpy(pkt.data+header_size, msg->data+cursor, pkt.data[0]);
-
-	/* send it out through the lower layer */
-	Sender_ToLowerLayer(&pkt);
+    end = true;
+    if (msg->size > cursor)
+    {
+        /* fill in the packet */
+        int size = msg->size - cursor;
+        memcpy(pkt.data + HEADERSIZE, msg->data + cursor, size);
+        package(pkt.data, size, first, end);
+        /* send it out through the lower layer */
+        if (canSend(pkt))
+        {
+            Sender_ToLowerLayer(&pkt);
+        }
+        else
+        {
+            addToBuffer(pkt);
+        }
     }
 }
 
