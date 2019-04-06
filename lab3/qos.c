d9b12b5c302e6ad4b78af708e1ef070d887a0790
derFischer <dingd2015@sjtu.edu.cn>
2019-04-07 00:38:37 +0800

    Update qos.c

--- lab3/qos.c
+++ lab3/qos.c
@@ -6,34 +6,34 @@
 #include "qos.h"
 
 #define CIR0 0
-#define CIR1 0
-#define CIR2 0
-#define CIR3 0
-
 #define CBS0 0
-#define CBS1 0
-#define CBS2 0
-#define CBS3 0
-
 #define EBS0 0
+
+#define CIR1 0
+#define CBS1 0
 #define EBS1 0
+
+#define CIR2 0
+#define CBS2 0
 #define EBS2 0
+
+#define CIR3 0
+#define CBS3 0
 #define EBS3 0
 
 #define GREENWQ 0
-#define YELLOWWQ 0
-#define REDWQ 0
-
 #define GREENMINTH 0
-#define YELLOWMINTH 0
-#define REDMINTH 0
-
 #define GREENMAXTH 0
-#define YELLOWMAXTH 0
-#define REDMAXTH 0
-
 #define GREENMAXP 0
+
+#define YELLOWWQ 0
+#define YELLOWMINTH 0
+#define YELLOWMAXTH 0
 #define YELLOWMAXP 0
+
+#define REDWQ 0
+#define REDMINTH 0
+#define REDMAXTH 0
 #define REDMAXP 0
 
 struct rte_meter_srtcm_params srtcmParams[APP_FLOWS_MAX];
@@ -42,32 +42,21 @@ struct rte_meter_srtcm srtcmFlows[APP_FLOWS_MAX];
 /**
  * srTCM
  */
-int qos_param_init(uint64_t CIR, uint64_t CBS, uint64_t EBS, int index)
+void qos_param_init(uint64_t CIR, uint64_t CBS, uint64_t EBS, int index)
 {
     srtcmParams[index].cir = CIR;
     srtcmParams[index].cbs = CBS;
     srtcmParams[index].ebs = EBS;
-    int ret;
-    ret = rte_meter_srtcm_config(&srtcmFlows[index], &srtcmParams[index]);
-    if (ret != 0)
-    {
-        rte_panic("qos_param_init error!\n");
-        return ret;
-    }
-    return 0;
+    rte_meter_srtcm_config(&srtcmFlows[index], &srtcmParams[index]);
 }
 
 int qos_meter_init(void)
 {
     /* to do */
-    if (qos_param_init(CIR0, CBS0, EBS0, 0) ||
-        qos_param_init(CIR1, CBS1, EBS1, 1) ||
-        qos_param_init(CIR2, CBS2, EBS2, 2) ||
-        qos_param_init(CIR3, CBS3, EBS3, 0))
-    {
-        rte_panic("qos_meter_init error!\n");
-        return -1;
-    }
+    qos_param_init(CIR0, CBS0, EBS0, 0);
+    qos_param_init(CIR1, CBS1, EBS1, 1);
+    qos_param_init(CIR2, CBS2, EBS2, 2);
+    qos_param_init(CIR3, CBS3, EBS3, 0);
     return 0;
 }
 
@@ -83,21 +72,13 @@ qos_meter_run(uint32_t flow_id, uint32_t pkt_len, uint64_t time)
  */
 struct rte_red_config redParams[e_RTE_METER_COLORS];
 struct rte_red redFlows[APP_FLOWS_MAX][e_RTE_METER_COLORS];
-int ret_params_init(const uint16_t wq_log2, const uint16_t min_th, const uint16_t max_th, const uint16_t maxp_inv, enum qos_color color)
-{
-    if (rte_red_config_init(&redParams[color], wq_log2, min_th, max_th, maxp_inv))
-    {
-        rte_panic("ret_params_init error!\n");
-        return -1;
-    }
 
+void ret_params_init(const uint16_t wq_log2, const uint16_t min_th, const uint16_t max_th, const uint16_t maxp_inv, enum qos_color color)
+{
+    rte_red_config_init(&redParams[color], wq_log2, min_th, max_th, maxp_inv);
     for (int i = 0; i < APP_FLOWS_MAX; i++)
     {
-        if (rte_red_rt_data_init(&app_red[i][color]))
-        {
-            rte_panic("ret_params_init error2!\n ");
-            return -1;
-        }
+        rte_red_rt_data_init(&app_red[i][color]);
     }
 }
 
@@ -110,7 +91,20 @@ int qos_dropper_init(void)
 
 }
 
+uint64_t time;
+unsigned queue[APP_FLOWS_MAX][e_RTE_METER_COLORS];
 int qos_dropper_run(uint32_t flow_id, enum qos_color color, uint64_t time)
 {
     /* to do */
+    if (app_time_stamp != time)
+    {
+        app_time_stamp = time;
+        red_queue_clear(time);
+    }
+
+    if (!rte_red_enqueue(&redParams[color], &redFlows[flow_id][color], queue[flow_id][color], time))
+    {
+        queue[flow_id][color] += 1;
+    }
+    return 0;
 }
